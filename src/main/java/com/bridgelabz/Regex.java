6cb6d52a87ef021d650e94e1560ec25e00b76d7e
dinesh nangare <dineshnangare1717@gmail.com>
2022-04-12 00:40:41 +0530

    adding first name

---
+++ src/main/java/com/bridgelabz/Regex.java
@@ -0,0 +1,10 @@
+package com.bridgelabz;
+
+public class Regex {
+    public static void main(String[] args) {
+            System.out.println("Welcome to regex program");
+            UserRegistration userRegistration = new UserRegistration();
+            userRegistration.firstName();
+
+        }
+    }
