239c4b3db1607a838a9072a583014b4f0c5b2444
dinesh nangare <dineshnangare1717@gmail.com>
2022-04-12 00:06:50 +0530

    adding first name

---
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -0,0 +1,31 @@
+package com.bridgelabz;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+
+//use of regex and patterns to solve user registration problem.
+
+import java.util.Scanner;
+
+
+public class UserRegistration {
+    static Scanner scanner = new Scanner(System.in);
+
+    //uc1
+    //adding valid first name
+
+    public void firstName() {
+        System.out.println("Enter First Name : : ");
+        String FirstName = scanner.next();
+        boolean answer = Pattern.matches("([A-Z]*[a-z]*){2,}", FirstName);
+
+        if (answer) {
+            System.out.println("thank you");
+        } else {
+            System.out.println("please enter valid first name ");
+        }
+
+    }
+    }
+
+    
